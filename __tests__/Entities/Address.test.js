4ea6adb21a1188cff0c11089a5ef2cb5b98ed928
alevshinskii <66357905+alevshinskii@users.noreply.github.com>
2022-09-09 12:24:09 +0300

    feat: address entity

---
+++ __tests__/Entities/Address.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+const Address = require('../../src/Entities/Address')
+
+const address = new Address()
+test('Should be able to create Address', () => {
+    expect(address).toBeDefined()
+})
+
+address.addressLine = 'new line 1'
+test('Should be able to get/set addressLine', () => {
+    expect(address.addressLine).toBe('new line 1')
+})
+
+address.addressLine2 = 'new line 2'
+test('Should be able to get/set addressLine2', () => {
+    expect(address.addressLine2).toBe('new line 2')
+})
+
+address.addressType = 1
+test('Should be able to get/set addressType', () => {
+    expect(address.addressType).toBe(1)
+})
+
+address.city = 'City'
+test('Should be able to get/set city', () => {
+    expect(address.city).toBe('City')
+})
+
+address.state = 'State'
+test('Should be able to get/set state', () => {
+    expect(address.state).toBe('State')
+})
+
+address.country = 'United States'
+test('Should be able to get/set country', () => {
+    expect(address.country).toBe('United States')
+})
--- src/Entities/Address.js
+++ src/Entities/Address.js
@@ -1,4 +1,70 @@
-class Address {}
+class Address {
+    constructor() {
+        this.addressLine = String.empty
+        this.addressLine2 = null
+        this.addressType = 0
+        this.city = String.empty
+        this.postalCode = String.empty
+        this.state = String.empty
+        this.country = String.empty
+    }
+
+    #addressLine
+    get addressLine() {
+        return this.#addressLine
+    }
+    set addressLine(value) {
+        this.#addressLine = value
+    }
+
+    #addressLine2
+    get addressLine2() {
+        return this.#addressLine2
+    }
+    set addressLine2(value) {
+        this.#addressLine2 = value
+    }
+
+    #addressType
+    get addressType() {
+        return this.#addressType
+    }
+    set addressType(value) {
+        this.#addressType = value
+    }
+
+    #city
+    get city() {
+        return this.#city
+    }
+    set city(value) {
+        this.#city = value
+    }
+
+    #postalCode
+    get postalCode() {
+        return this.#postalCode
+    }
+    set postalCode(value) {
+        this.#postalCode = value
+    }
+
+    #state
+    get state() {
+        return this.#state
+    }
+    set state(value) {
+        this.#state = value
+    }
+
+    #country
+    get country() {
+        return this.#country
+    }
+    set country(value) {
+        this.#country = value
+    }
+}
 
 // eslint-disable-next-line no-undef
 module.exports = Address

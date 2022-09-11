e6a01a830e2bc0ad71f9ed35ee55760a33141c71
alevshinskii <66357905+alevshinskii@users.noreply.github.com>
2022-09-11 20:33:45 +0300

    fix: CustomerValidator.test.js tests fixed

--- __tests__/Validators/CustomerValidator.test.js
+++ __tests__/Validators/CustomerValidator.test.js
@@ -8,16 +8,16 @@ test('Should be able to use CustomerValidator', () => {
   expect(CustomerValidator.Validate(new Customer())).toBeDefined();
 });
 const address = new Address();
-const validCustomer = new Customer({
-  firstName: 'first name',
-  lastName: 'last name',
-  addresses: [address, address],
-  phoneNumber: '123456789012345',
-  email: 'email@gmail.com',
-  notes: ['new note'],
-  totalPurchasesAmount: 1000,
-  lastPurchaseDate: new Date(2022, 1, 1),
-});
+const validCustomer = new Customer();
+
+validCustomer.firstName = 'first name';
+validCustomer.lastName = 'last name';
+validCustomer.addresses = [address, address];
+validCustomer.phoneNumber = '123456789012345';
+validCustomer.email = 'email@gmail.com';
+validCustomer.notes = ['new note'];
+validCustomer.totalPurchasesAmount = 1000;
+validCustomer.lastPurchaseDate = new Date(2022, 1, 1);
 
 test('Should be able to get no errors validating valid customer', () => {
   expect(CustomerValidator.Validate(validCustomer).length).toBe(0);
